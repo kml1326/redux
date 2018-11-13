f0c589bda8ae85ffb33a6481f2545a32f7ebd723
kml1326 <kmlraj131996@gmail.com>
2018-11-13 19:18:40 +0530

    increment-decrement

---
+++ increment/decrement/src/reducers/index.js
@@ -0,0 +1,12 @@
+var initialState = {count: 0};
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {count: state.count + 1}
+        case 'DECREMENT':
+            return {count: state.count - 1};
+        default:
+            return state;
+    }
+}
