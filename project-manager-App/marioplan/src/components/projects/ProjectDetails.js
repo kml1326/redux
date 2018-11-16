2c7b7042a458764288a0b5d426de702898104590
kml1326 <kmlraj131996@gmail.com>
2018-11-17 01:01:49 +0530

    initial commit of project-manager

---
+++ project-manager-App/marioplan/src/components/projects/ProjectDetails.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ProjectDetails = () => {
+  return (
+		<div className='container section project-details'>
+			<div className='card z-depth-0'>
+				<div className='card-content'>
+					<span className='.card-title'>Project Title</span>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+				</div>
+				<div className='card-action gret lighten-4 grey-text'>
+					<div>Posted by The Komal Raj</div>
+					<div>2nd September, 2am</div>
+				</div>
+			</div>
+		</div>
+	);  
+}
+
+export default ProjectDetails;
---
+++ project-manager-App/marioplan/src/store/reducers/projectReducer.js
@@ -0,0 +1,13 @@
+const initState = {
+  projects: [
+    {id: '1', title: 'help me find peach', content: 'blah blah blah blah'},
+    {id: '1', title: 'collect all the stars', content: 'blah blah blah blah'},
+    {id: '1', title: 'egg hunt with yoshi', content: 'blah blah blah blah'}
+  ]
+}
+
+const projectReducer = (state = initState, action) => {
+  return state
+}
+
+export default projectReducer;
